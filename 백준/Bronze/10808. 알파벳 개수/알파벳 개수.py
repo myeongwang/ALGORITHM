e002e99956b1b802ddc76c66c2e2b68e9f61638d
MYEONGJIN LEE <46295610+myeongwang@users.noreply.github.com>
2023-08-12 23:19:50 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.py
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.py
@@ -1,19 +1,17 @@
+#boj 10808 array 복습
+
 def count_alphabet(s):
-    alphabet_count = [0] * 26  # 알파벳 개수를 저장할 리스트 초기화
+  alphabet_count=[0]*26
 
-    for c in s:
-        if 'a' <= c <= 'z':
-            index = ord(c) - ord('a')  # 알파벳이면 인덱스 계산
-            alphabet_count[index] += 1  # 해당 알파벳 개수 증가
+  for c in s:
+    if 'a'<=c<='z':
+      index = ord(c)-ord('a')
+      alphabet_count[index]+=1
 
-    return alphabet_count
+  return alphabet_count    
+  
 
-def main():
-    input_string = input().strip().lower()  # 문자열 입력 (소문자로 변환)
-    result = count_alphabet(input_string)
+input_string = input().strip().lower()
+result = count_alphabet(input_string)
 
-    for count in result:
-        print(count, end=' ')
-
-if __name__ == "__main__":
-    main()
+print(' '.join(map(str,result)))
