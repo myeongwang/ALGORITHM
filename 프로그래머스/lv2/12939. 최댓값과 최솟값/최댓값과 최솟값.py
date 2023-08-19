c1383e12f7f6e5820b35960c72354582a4501209
MYEONGJIN LEE <46295610+myeongwang@users.noreply.github.com>
2023-08-19 12:36:10 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
@@ -1,6 +1,25 @@
+"""
 def solution(s):
     s_list=s.split()
     s_list=list(map(int,s_list))
     s_list.sort()
     
     return (str(s_list[0])+" "+str(s_list[len(s_list)-1]))
+ """
+
+def solution(s):
+    
+    s_list=s.split()
+    s_list=list(map(int,s_list))
+    s_list.sort()
+    return (str(s_list[0])+" "+str(s_list[len(s_list)-1]))
+
+
+
+
+
+
+
+
+
+
