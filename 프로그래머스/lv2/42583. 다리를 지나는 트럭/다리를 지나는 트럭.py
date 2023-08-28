07ed6ab9f10c69b7f033ddf4931f8d711f87e399
MYEONGJIN LEE <46295610+myeongwang@users.noreply.github.com>
2023-08-28 20:32:03 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
+++ 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -8,7 +8,14 @@ def solution(bridge_length, weight, truck_weights):
         if on_bridge and time - on_bridge[0][1] == bridge_length:
             on_bridge.pop(0)  # 다리에서 나가는 트럭 처리
         
-        if sum([truck[0] for truck in on_bridge]) + truck_weights[0] <= weight:
+        # 다리 위에 있는 트럭들의 무게의 합을 계산합니다.
+        total_weight_on_bridge = 0
+        for truck in on_bridge:
+            total_weight_on_bridge += truck[0]
+
+        # 현재 다리에 올리려는 트럭의 무게를 더해도 다리가 견딜 수 있는 무게를 넘지 않는지 확인합니다.
+        if total_weight_on_bridge + truck_weights[0] <= weight:
+            # 트럭을 다리 위에 올릴 수 있습니다.
             truck = truck_weights.pop(0)
             on_bridge.append((truck, time))  # 다리에 트럭 추가
     
