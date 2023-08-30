40fb2d9cc9f1d01416c1ae13fc8a45dcad8e5739
MYEONGJIN LEE <46295610+myeongwang@users.noreply.github.com>
2023-08-30 22:00:44 +0900

    [level 3] Title: 풍선 터트리기, Time: 340.41 ms, Memory: 63.7 MB -BaekjoonHub

--- 프로그래머스/lv3/68646. 풍선 터트리기/풍선 터트리기.py
+++ 프로그래머스/lv3/68646. 풍선 터트리기/풍선 터트리기.py
@@ -1,37 +1,27 @@
 def solution(a):
-    answer = 2  # 처음과 끝의 풍선은 항상 남을 것이므로 2개부터 시작
+    answer =2
     
-    if len(a) <= 2:
+    if len(a)<=2:
         return len(a)
     
-    left_min = a[0]  # 현재 위치까지의 최소값
-    right_min = a[-1]  # 현재 위치부터 끝까지의 최소값
+    left_min=a[0]
+    right_min=a[-1]
     
-    left_checked = [0] * len(a)  # 왼쪽부터 풍선을 터트렸을 때 남는지 여부를 기록하는 리스트
-    right_checked = [0] * len(a)  # 오른쪽부터 풍선을 터트렸을 때 남는지 여부를 기록하는 리스트
+    left_checked=[0]*len(a)
+    right_checked=[0]*len(a)
     
-    for i in range(1, len(a) - 1):
-        if a[i] < left_min:
+    for i in range(1,len(a)-1):
+        if a[i]< left_min:
             left_min = a[i]
-            left_checked[i] = 1
+            left_checked[i]=1 
             
-        if a[-i - 1] < right_min:
-            right_min = a[-i - 1]
-            right_checked[-i - 1] = 1
-    
+        if a[-i-1]<right_min:
+            right_min=a[-i-1]
+            right_checked[-i-1]=1
+            
     for i in range(len(a)):
-        if left_checked[i] or right_checked[i]:
-            answer += 1
+        if left_checked[i] or right_checked[i]: #같은 표현 left_checked[i]==True or right_checked[i]==True
+            answer+=1                               
+    return answer         
     
-    return answer
-
-
-
-
-
-
-
-
-
-
-
+    #left_checked[i]와 right_checked[i]에서의 1의 갯수 - 공통된 위치의 1의 갯수와 answer은 같은 값
