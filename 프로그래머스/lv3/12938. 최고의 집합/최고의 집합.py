78d0daf03eb061874f5b3b2962fe577e8a162582
MYEONGJIN LEE <46295610+myeongwang@users.noreply.github.com>
2023-08-31 20:38:18 +0900

    [level 3] Title: 최고의 집합, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv3/12938. 최고의 집합/최고의 집합.py
+++ 프로그래머스/lv3/12938. 최고의 집합/최고의 집합.py
@@ -1,13 +1,19 @@
 def solution(n, s):
+    answer = []
+    
     if n > s:
         return [-1]
     
     quotient = s // n
     remainder = s % n
-    
-    answer = [quotient] * n
-    
-    for i in range(remainder):
-        answer[-(i + 1)] += 1
-    
+         
+    if n == 2 and quotient * 2 + remainder == s:    
+        answer = [quotient, remainder + quotient] 
+    else:
+        if remainder == 0:
+            answer = [quotient] * n
+        else:
+            if remainder >= 1:
+                answer = [quotient] * (n - remainder) + [quotient + 1] * remainder
+                  
     return answer
