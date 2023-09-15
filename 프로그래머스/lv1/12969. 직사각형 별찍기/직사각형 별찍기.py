f9dab38a478e217b3d01a45f674f6d1fb62ee5d1
MYEONGJIN LEE <46295610+myeongwang@users.noreply.github.com>
2023-09-15 22:10:44 +0900

    [level 1] Title: 직사각형 별찍기, Time: 12.09 ms, Memory: 7.47 MB -BaekjoonHub

--- 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.py
+++ 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.py
@@ -1,4 +1,6 @@
-n, m = map(int, input().split())  # 두 개의 정수 n과 m을 입력받음
+n,m=map(int,input().split())
+i=0
 
-for _ in range(m):  # 세로의 길이 m만큼 반복
-    print('*' * n)  # 가로의 길이 n만큼 별(*) 문자 출력
+while i<m:
+    print("*"*n)
+    i=i+1
