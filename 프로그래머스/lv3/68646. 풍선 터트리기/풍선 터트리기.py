1236db461f17e9fcb9b6ef4be3995415af385a1d
MYEONGJIN LEE <46295610+myeongwang@users.noreply.github.com>
2023-09-17 23:22:01 +0900

    [level 3] Title: 풍선 터트리기, Time: 353.97 ms, Memory: 63.9 MB -BaekjoonHub

--- 프로그래머스/lv3/68646. 풍선 터트리기/풍선 터트리기.py
+++ 프로그래머스/lv3/68646. 풍선 터트리기/풍선 터트리기.py
@@ -13,15 +13,15 @@ def solution(a):
     for i in range(1,len(a)-1):
         if a[i]< left_min:
             left_min = a[i]
-            left_checked[i]=1 
+            left_checked[i]=1
             
-        if a[-i-1]<right_min:
-            right_min=a[-i-1]
+        if a[-i-1]< right_min:
+            right_min = a[-i-1]
             right_checked[-i-1]=1
             
     for i in range(len(a)):
-        if left_checked[i] or right_checked[i]: #같은 표현 left_checked[i]==True or right_checked[i]==True
+        if left_checked[i] or right_checked[i]: 
             answer+=1                               
-    return answer         
-    
-    #left_checked[i]와 right_checked[i]에서의 1의 갯수 - 공통된 위치의 1의 갯수와 answer은 같은 값
+    return answer     
+        
+        
