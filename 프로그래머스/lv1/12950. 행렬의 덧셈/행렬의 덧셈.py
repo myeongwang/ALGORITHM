7f3514aaea19c15eee50df37433791d3ca127dd9
MYEONGJIN LEE <46295610+myeongwang@users.noreply.github.com>
2023-09-19 10:29:03 +0900

    [level 1] Title: 행렬의 덧셈, Time: 32.14 ms, Memory: 41.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.py
+++ 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.py
@@ -1,12 +1,9 @@
 import numpy as np
 
 def solution(A,B):
-    A_np = np.array(A)
-    B_np = np.array(B)
-    result = A_np + B_np
+    arr1=np.array(A)
+    arr2=np.array(B)
+    result = arr1+arr2
     
-    numpy_array = np.array([1, 2, 3, 4, 5])
-    result_1 = numpy_array.tolist()
-
-    print(result_1)
     return result.tolist()
+    
