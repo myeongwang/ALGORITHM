38d61e557b98839df2fabc658388b27a81dba788
MYEONGJIN LEE <46295610+myeongwang@users.noreply.github.com>
2023-09-19 14:33:46 +0900

    [level 2] Title: 줄 서는 방법, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12936. 줄 서는 방법/줄 서는 방법.py
+++ 프로그래머스/lv2/12936. 줄 서는 방법/줄 서는 방법.py
@@ -6,7 +6,9 @@ def solution(n, k):
     perms = list(permutations(numbers))
 
     answer = perms[k - 1]
-    return answer"""
+    return answer
+    """
+"""
 
 import math
 
@@ -21,4 +23,17 @@ def solution(n, k):
         answer.append(numbers.pop(index))
         k %= math.factorial(n - 1 - i)
 
+    return answer
+"""
+from math import factorial
+
+def solution(n, k):
+   
+    answer = []
+    order = list(range(1,n+1))
+    while n!=0 :
+        fact = factorial(n-1)
+        #answer.append(order.pop(k//fact-1 if k%fact ==0 else k//fact))
+        answer.append(order.pop((k-1)//fact))
+        n,k = n-1, k%fact
     return answer
