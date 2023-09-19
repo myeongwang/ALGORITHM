ca168e99afcfb8cbc2eba06615c664290df62eae
MYEONGJIN LEE <46295610+myeongwang@users.noreply.github.com>
2023-09-19 13:37:58 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.py
+++ 프로그래머스/lv2/42842. 카펫/카펫.py
@@ -1,10 +1,12 @@
 import math
 
 def solution(brown, yellow):
-    total_size = brown + yellow
+    sum = brown + yellow
     
-    for h in range(3, int(math.sqrt(total_size)) + 1):
-        if total_size % h == 0:
-            w = total_size // h
-            if (w - 2) * (h - 2) == yellow:
-                return [w, h]
+    for h in range(3,int(math.sqrt(sum))+1):
+        if sum% h ==0:
+            w= sum//h
+            if (w-2)*(h-2) == yellow:
+                return [w,h]
+    
+ 
