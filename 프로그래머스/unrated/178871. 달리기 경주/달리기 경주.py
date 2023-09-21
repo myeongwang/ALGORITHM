0b2ea9b2ae8b7ecc06f5d77d3c080dce4830411e
MYEONGJIN LEE <46295610+myeongwang@users.noreply.github.com>
2023-09-21 21:49:52 +0900

    [unrated] Title: 달리기 경주, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.py
+++ 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.py
@@ -1,11 +1,13 @@
 def solution(players, callings):
-    index_dict = {player: i for i, player in enumerate(players)}
     
+    index_dict={player: i for i,player in enumerate(players)} #enumerate는(index,value) 
+                                                              #dict는 {value,index} 라는걸 기억합시다
     for calling in callings:
         i = index_dict[calling]
-        players[i], players[i-1] = players[i-1], players[i]
-        
-        index_dict[players[i]] = i
-        index_dict[players[i-1]] = i - 1
+        players[i], players[i-1] = players[i-1], players[i] # 변수 스왑 직접 지원 
+                                                            # 변수 스왑하는 표준적인 방법 ㅅ2ㅂ
+                                                            #풀어쓰는거랑 다름
+        index_dict[players[i]] =i
+        index_dict[players[i-1]] =i-1
         
     return players
