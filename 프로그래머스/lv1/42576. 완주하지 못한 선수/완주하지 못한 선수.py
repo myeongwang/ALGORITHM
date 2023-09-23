3297f01c9710556e96e35ff89935142b7b2ffe90
MYEONGJIN LEE <46295610+myeongwang@users.noreply.github.com>
2023-09-23 14:23:31 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 52.18 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,4 +1,22 @@
 def solution(participant, completion):
+    
+    participant_dict={}
+    
+    for p in participant:
+        if p in participant_dict:
+            participant_dict[p]+=1 #이미 있는 경우(동명이인)
+        else:
+            participant_dict[p]=1
+            
+    for c in completion:
+        if c in participant_dict:
+            participant_dict[c]-=1
+            if participant_dict[c]==0:
+                del participant_dict[c]
+                
+    return list(participant_dict.keys())[0]
+    
+"""
     participant_dict = {}  # 참가자를 딕셔너리로 변환
     
     for p in participant: #리스트 순회하며 딕셔너리에 추가, 이미 있는 경우 등장회수 증가
@@ -16,3 +34,7 @@ def solution(participant, completion):
     
    
     return list(participant_dict.keys())[0]
+"""
+
+
+
