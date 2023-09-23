bfc5283d34a355ca9d71cd0bac7ede964e6e98ba
MYEONGJIN LEE <46295610+myeongwang@users.noreply.github.com>
2023-09-23 23:44:38 +0900

    [level 1] Title: 폰켓몬, Time: 1.36 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.py
@@ -1,15 +1,25 @@
 def solution(nums):
-    # 해시맵을 사용하여 각 폰켓몬 종류의 개수를 저장
-    pokemon_count = {}
+    pockemon_dict={}
     
-    for num in nums:
-        if num in pokemon_count:
-            pokemon_count[num] += 1
+    for n in nums:
+        if n not in pockemon_dict:
+            pockemon_dict[n]=1
         else:
-            pokemon_count[num] = 1
-    print(pokemon_count)
-    print(len(pokemon_count))
-    # 최대 종류의 개수를 계산
-    max_kinds = min(len(nums) // 2, len(pokemon_count))
+            pockemon_dict[n]+=1
+  
+            
+    return min(len(nums)/2 , len(pockemon_dict) )       
+
+
+# 아래의 set 이용하면 해시의 len 사용할 필요없이 바로 중복 제거
+"""
+
+def solution(nums):
+    # N/2 마리의 폰켓몬을 선택해야 하므로 선택할 수 있는 최대 종류의 개수는 N/2입니다.
+    max_kinds = len(nums) // 2
     
-    return max_kinds
+    # 폰켓몬 종류 번호를 set으로 변환하여 중복을 제거합니다.
+    unique_pokemons = set(nums)
+    
+    # 중복을 제거한 폰켓몬 종류 수와 최대 종류 수 중 작은 값을 반환합니다.
+    return min(max_kinds, len(unique_pokemons)) """
