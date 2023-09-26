c3ff72d78d7eef6e5283fda5b153faa34155e112
MYEONGJIN LEE <46295610+myeongwang@users.noreply.github.com>
2023-09-26 22:03:42 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -13,9 +13,9 @@ def solution(operations):
             if operand < 0:
                 heapq.heappop(heap)
             else:
-                heap.remove(max(heap))
+                heap.remove(max(heap)) # heappop(heap,max(heap))은 사용 불가 
 
     if not heap:
         return [0, 0]
 
-    return [max(heap), heap[0]]
+    return [max(heap), min(heap)]
