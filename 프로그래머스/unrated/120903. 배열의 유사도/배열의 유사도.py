af91317cf04624936ab6f0f8078a83bd5ceb034a
PLUTO <46295610+myeongwang@users.noreply.github.com>
2023-10-31 01:57:07 +0900

    [level unrated] Title: 배열의 유사도, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/120903. 배열의 유사도/배열의 유사도.py
+++ 프로그래머스/unrated/120903. 배열의 유사도/배열의 유사도.py
@@ -1,8 +1,13 @@
+def solution(s1,s2):
+    return len(set(s1) & set(s2))
+
+
+"""
 def solution(s1, s2):
     count = 0
     for string in s1:
         count += s2.count(string)
     return count
-
+"""
 
 
