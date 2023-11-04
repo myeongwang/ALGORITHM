cbbc344cdccd592e04ab79cb41a5534cd9c2afd3
PLUTO <46295610+myeongwang@users.noreply.github.com>
2023-11-05 01:12:01 +0900

    [level unrated] Title: 369게임, Time: 0.08 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/unrated/120891. 369게임/369게임.py
+++ 프로그래머스/unrated/120891. 369게임/369게임.py
@@ -1,3 +1,16 @@
+import re
+def solution(order):
+    order= str(order)
+    return len(re.findall('[369]',order)) #re.findall 함수는 주어진 패턴과 일치하는 모든 부분 문자열을 리스트로 반환
+
+
+
+
+"""
+def solution(order):
+    order = str(order)
+    return order.count('3') + order.count('6') + order.count('9')
+
 def solution(order):
     order = list(str(order))
     count=0
@@ -7,5 +20,5 @@ def solution(order):
             count += 1
             
     return count
-
+"""
            
