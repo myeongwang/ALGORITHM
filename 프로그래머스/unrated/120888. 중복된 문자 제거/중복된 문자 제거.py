4f88a8e221d9f04d0c6e86eb4e562796ee4f7b1a
PLUTO <46295610+myeongwang@users.noreply.github.com>
2023-11-05 01:25:33 +0900

    [level unrated] Title: 중복된 문자 제거, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/120888. 중복된 문자 제거/중복된 문자 제거.py
+++ 프로그래머스/unrated/120888. 중복된 문자 제거/중복된 문자 제거.py
@@ -1,7 +1,17 @@
 def solution(my_string):
+    print(dict.fromkeys(my_string))
     return ''.join(dict.fromkeys(my_string))
 
 
 
 #key가 아닌 value 반환 원할 시
 #my_dict = dict.fromkeys(my_string, 'value')
+
+"""
+def solution(my_string):
+    answer = ''
+    for i in my_string:
+        if i not in answer:
+            answer+=i
+    return answer
+"""
