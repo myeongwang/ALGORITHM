059988f4b608e2ad5e8d099d6bed71b652b6dbc7
PLUTO <46295610+myeongwang@users.noreply.github.com>
2023-11-05 00:30:11 +0900

    [level unrated] Title: 외계행성의 나이, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/unrated/120834. 외계행성의 나이/외계행성의 나이.py
+++ 프로그래머스/unrated/120834. 외계행성의 나이/외계행성의 나이.py
@@ -1,3 +1,12 @@
 def solution(age):
+    conv = {'0':'a','1':'b','2':'c','3':'d','4':'e'
+            ,'5':'f','6':'g','7':'h','8':'i','9':'j'}
+    return ''.join(conv[i] for i in str(age))
+
+
+
+"""
+def solution(age):
     answer = 'abcdefghij'
     return ''.join(map(lambda x: answer[int(x)],str(age)))
+    """
