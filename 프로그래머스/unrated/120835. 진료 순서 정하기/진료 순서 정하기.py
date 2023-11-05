6ae9d2ba3fadc37311899bd9b8b6a4b9703c9ed1
PLUTO <46295610+myeongwang@users.noreply.github.com>
2023-11-05 16:52:51 +0900

    [level unrated] Title: 진료 순서 정하기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120835. 진료 순서 정하기/진료 순서 정하기.py
@@ -0,0 +1,7 @@
+def solution(emergency):
+    dic = {}
+    
+    for char in emergency:
+        dic[char] = sorted(emergency, reverse=True).index(char) + 1
+
+    return [dic[num] for num in emergency]
