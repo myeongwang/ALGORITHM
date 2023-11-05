063781efd1c237e35f4957d38cff7f9306e215b2
PLUTO <46295610+myeongwang@users.noreply.github.com>
2023-11-05 16:23:11 +0900

    Update 한 번만 등장한 문자.py

--- 프로그래머스/unrated/120896. 한 번만 등장한 문자/한 번만 등장한 문자.py
+++ 프로그래머스/unrated/120896. 한 번만 등장한 문자/한 번만 등장한 문자.py
@@ -1,3 +1,19 @@
+#3.
+from collections import Counter
+
+def solution(s):
+    string = []
+    for i,j in Counter(s).items():
+        if j ==1:
+            string.append(i)
+    return ''.join(sorted(string))        
+    
+"""
+#1
+def solution(s):
+    return ''.join(sorted([c for c in s if s.count(c)==1]))
+    
+#2
 def solution(s):
     dic={}
     
@@ -8,5 +24,5 @@ def solution(s):
             dic[char]=1
     #print(sorted([char for char, count in dic.items() if count ==1 ]))        
     return ''.join(sorted([char for char, count in dic.items() if count ==1 ]))
-    
-    
+    #.items: 딕셔너리 값 반환 
+"""    
