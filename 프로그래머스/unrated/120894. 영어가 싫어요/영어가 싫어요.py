536bfbe4d1a22ef0a595360b86064be36095d19a
PLUTO <46295610+myeongwang@users.noreply.github.com>
2023-11-05 17:23:37 +0900

    [level unrated] Title: 영어가 싫어요, Time: 0.20 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120894. 영어가 싫어요/영어가 싫어요.py
@@ -0,0 +1,18 @@
+import re
+
+def solution(numbers):
+    s=''
+    dic={'zero':'0',
+         'one':'1',
+         'two':'2',
+         'three':'3',
+         'four':'4',
+         'five':'5',
+         'six':'6',
+         'seven':'7',
+         'eight':'8',
+         'nine':'9'
+    }
+    for i in re.findall(r'(zero|one|two|three|four|five|six|seven|eight|nine)',numbers):
+    	s+=dic[i]
+    return int(s)
