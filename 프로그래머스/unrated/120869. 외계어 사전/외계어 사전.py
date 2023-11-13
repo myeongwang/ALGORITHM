cbff6dbb292f44b3d4e1987f5ee23b218462c849
PLUTO <46295610+myeongwang@users.noreply.github.com>
2023-11-13 23:42:38 +0900

    [level unrated] Title: 외계어 사전, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/120869. 외계어 사전/외계어 사전.py
+++ 프로그래머스/unrated/120869. 외계어 사전/외계어 사전.py
@@ -1,5 +1,16 @@
 def solution(spell, dic):
+    spell = set(spell)
+    for s in dic:
+        if not spell-set(s): #spell 문자열의 집합에서 s 문자열의 집합을 빼고 결과 집합이 비어있는지 확인
+            return 1
+    return 2
+
+"""
+def solution(spell, dic):
+    
     for d in dic:
-        if sorted(d) == sorted(spell):
+        if sorted(d)==sorted(spell):
             return 1
     return 2    
+    
+    """
