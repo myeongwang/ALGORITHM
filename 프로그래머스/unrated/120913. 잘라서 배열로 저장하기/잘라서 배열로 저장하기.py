016680da783f8bdebf47408c4eebe15a408b2f8e
PLUTO <46295610+myeongwang@users.noreply.github.com>
2023-11-14 00:40:19 +0900

    [level unrated] Title: 잘라서 배열로 저장하기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.py
+++ 프로그래머스/unrated/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.py
@@ -1,7 +1,8 @@
 def solution(my_str, n):
-    result = []
-    for i in range(0, len(my_str), n):
+    result= []
+    
+    for i in range(0,len(my_str),n):
         result.append(my_str[i:i+n])
-    return result
+    return result    
             
             
