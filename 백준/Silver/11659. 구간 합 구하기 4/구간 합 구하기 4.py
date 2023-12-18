6bf126c1eb36070eb91dcf145ac8bf0da466d395
PLUTO <46295610+myeongwang@users.noreply.github.com>
2023-12-19 01:33:59 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 228 ms, Memory: 41144 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
@@ -1,9 +1,11 @@
 import sys
+input = sys.stdin.readline
 
-N, M = map(int, sys.stdin.readline().split())
-arr = list(map(int, sys.stdin.readline().split()))
+n,m = map(int,input().split())
+arr = list(map(int,input().split()))
+
 prefix_sum = [0]
-temp =0
+temp = 0
 
 # 각 구간의 합을 저장하는 배열 생성
 for i in arr:
@@ -11,10 +13,10 @@ for i in arr:
     prefix_sum.append(temp)
    
 # 구간 합 계산 및 출력
-for i in range(M):
-    s,e = map(int,sys.stdin.readline().split())
+for i in range(m):
+    s,e = map(int,input().split())
     print(prefix_sum[e]-prefix_sum[s-1])
     
     
-
-
+    
+    
