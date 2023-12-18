22c5c7fe25c7cd71504c06a5d1d18cbdbf002e90
PLUTO <46295610+myeongwang@users.noreply.github.com>
2023-12-19 01:00:28 +0900

    [Silver V] Title: 수들의 합 5, Time: 4464 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2018. 수들의 합 5/수들의 합 5.py
+++ 백준/Silver/2018. 수들의 합 5/수들의 합 5.py
@@ -5,15 +5,16 @@ end_index=1
 sum=1
 
 while end_index !=n:
-    if sum ==n:
-        count+=1
-        end_index+=1
-        sum+=end_index
-    elif sum >n:
-        sum-=start_index
-        start_index +=1
-    else:
-        end_index+=1
-        sum+=end_index
-        
+   if sum < n:
+    end_index +=1
+    sum+=end_index
+   elif sum >n:
+    sum-=start_index
+    start_index+=1
+   
+   else:
+    count+=1
+    end_index+=1
+    sum+=end_index
+    
 print(count)        
