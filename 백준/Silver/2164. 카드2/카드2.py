9d4dead9ce11c57dd542226e64555d873ba93ded
PLUTO <46295610+myeongwang@users.noreply.github.com>
2023-12-20 01:47:58 +0900

    [Silver IV] Title: 카드2, Time: 188 ms, Memory: 50964 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -0,0 +1,14 @@
+from collections import deque
+
+n= int(input())
+myqueue = deque()
+
+for i in range(1,n+1):
+    myqueue.append(i)
+    
+while len(myqueue)>1:
+    myqueue.popleft()
+    myqueue.append(myqueue.popleft())
+    
+print(myqueue[0])    
+
