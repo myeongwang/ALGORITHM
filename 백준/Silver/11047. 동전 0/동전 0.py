b5abadb6a3ccb9574422536632670b1c174b4c60
PLUTO <46295610+myeongwang@users.noreply.github.com>
2024-01-03 17:46:23 +0900

    [Silver IV] Title: 동전 0, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -1,17 +1,25 @@
-def solution(inputPriceArr):
-        global Price
-        answer = 0
-        for i in range(InputLen):
-            if Price // inputPriceArr[i] > 0:
-                answer += (Price // inputPriceArr[i])
-                Price -= inputPriceArr[i] * (Price // inputPriceArr[i])
-                
-        return answer        
-        
-InputLen, Price= map(int, input().split())
-inputPriceArr = []
-for i in range(InputLen):
-    inputPriceArr.append(int(input()))
- 
-inputPriceArr.sort(reverse=True)
-print(solution(inputPriceArr))
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+coin_values = [int(input()) for _ in range(N)]
+
+def min_coin_count(N, M, coin_values):
+    coin_values.sort(reverse=True)  # 동전 가치를 내림차순으로 정렬
+
+    count = 0  # 동전 개수를 저장할 변수
+
+    for coin in coin_values:
+        if M >= coin:
+            num_coins = M // coin
+            count += num_coins
+            M -= num_coins * coin
+
+    if M != 0:
+        return -1  # 남은 잔돈을 모두 동전으로 바꿀 수 없는 경우
+
+    return count
+
+# 함수 실행
+result = min_coin_count(N, M, coin_values)
+print(result)
