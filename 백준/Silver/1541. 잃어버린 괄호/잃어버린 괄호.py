68889ee41c67eeea81034bca4667da12a5ce519f
PLUTO <46295610+myeongwang@users.noreply.github.com>
2024-01-04 13:11:47 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -1,9 +1,9 @@
-expression = input().strip() 
-parts = expression.split('-')
+expression = input().strip()  # 입력 문자열을 읽어옵니다.
+parts = expression.split('-')  # '-'를 기준으로 수식을 나눕니다.
 
 total = sum(map(int,parts[0].split('+')))
 
 for part in parts[1:]:
-    total -= sum(map(int,part.split('+')))
-    
-print(total)    
+    total-=sum(map(int,part.split('+')))
+
+print(total)
