d50bf3571f7e2647b214b30baeb20807a255dca1
PLUTO <46295610+myeongwang@users.noreply.github.com>
2024-01-04 17:34:36 +0900

    [Silver III] Title: 01타일, Time: 352 ms, Memory: 69652 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+dp = [0] * (N + 1)
+
+dp[1] = 1
+if N > 1:
+    dp[2] = 2
+for i in range(3, N + 1):
+    dp[i] = (dp[i - 1] + dp[i - 2]) % 15746
+
+print(dp[N])
