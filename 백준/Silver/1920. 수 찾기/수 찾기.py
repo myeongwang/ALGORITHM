5469d40fcf7c81e4312133f5f916cff5a9464932
PLUTO <46295610+myeongwang@users.noreply.github.com>
2024-01-07 23:43:30 +0900

    [Silver IV] Title: 수 찾기, Time: 428 ms, Memory: 46712 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,32 +1,20 @@
-import sys
-input = sys.stdin.readline
-
-N = int(input())
-A = list(map(int, input().split()))
-A.sort()
-M = int(input())
-target_list = list(map(int, input().split()))
-
-for i in range(M):
-    find = False
-    target = target_list[i]
-    
-    start = 0
-    end = len(A) - 1
-    
+def binary_search(array, target, start, end):
     while start <= end:
         mid = (start + end) // 2
-        midv = A[mid]
-        
-        if midv > target:
+        if array[mid] == target:
+            return 1
+        elif array[mid] > target:
             end = mid - 1
-        elif midv < target:
-            start = mid + 1
         else:
-            find = True
-            break
-            
-    if find:
-        print(1)
-    else:
-        print(0)
+            start = mid + 1
+    return 0
+
+N = int(input())
+array = list(map(int, input().split()))
+array.sort()
+
+M = int(input())
+targets = list(map(int, input().split()))
+
+for target in targets:
+    print(binary_search(array, target, 0, N - 1))
