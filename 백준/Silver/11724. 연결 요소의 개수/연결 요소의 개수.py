238c68426bfeaf300332e5f1ac67be33463f657e
PLUTO <46295610+myeongwang@users.noreply.github.com>
2024-03-23 00:51:42 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 664 ms, Memory: 65156 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -3,25 +3,35 @@ sys.setrecursionlimit(10000)
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-A = [[] for _ in range(n + 1)]  # 인접 리스트 초기화
-visited = [False] * (n + 1)  # 방문 리스트 초기화
+posco = [[]for _ in range(n+1)]
+asml = [False]* (n+1)
 
 def DFS(v):
-    visited[v] = True
-    for i in A[v]:
-        if not visited[i]:
+    asml[v]= True
+    for i in posco[v]:
+        if not asml[i]:
             DFS(i)
-
+            
+            
 for _ in range(m):
-    s, e = map(int, input().split())
-    A[s].append(e)
-    A[e].append(s)
-
+    s,e = map(int, input().split())
+    posco[s].append(e)
+    posco[e].append(s)
+    
 count = 0
 
-for i in range(1, n + 1):
-    if not visited[i]:  # 연결노드 중 방문하지 않았던 노드만 탐색
-        count += 1
+for i in range(1,n+1):
+    if not asml[i]:
+        count +=1
         DFS(i)
-
-print(count)
+        
+print(count)        
+            
+        
+    
+    
+    
+    
+    
+    
+    
