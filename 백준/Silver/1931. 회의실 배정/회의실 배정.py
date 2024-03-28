048a6bd206baa1b1f24eb781f26da3747dc8f2f7
PLUTO <46295610+myeongwang@users.noreply.github.com>
2024-03-28 10:35:49 +0900

    [Silver I] Title: 회의실 배정, Time: 276 ms, Memory: 46628 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -0,0 +1,20 @@
+import sys 
+input = sys.stdin.readline
+n= int(input())
+arr= [[0]*2 for _ in range(n)]
+
+for i in range(n):
+      s,e = map(int,input().split())
+      arr[i][0] = e 
+      arr[i][1] = s
+      
+arr.sort()
+count=0
+end = 0
+      
+for i in range(n):
+      if arr[i][1]>= end:
+          end = arr[i][0]
+          count +=1
+print(count)      
+      
