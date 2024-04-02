3d088be99e202067568029ef836a724cfe01a14d
PLUTO <46295610+myeongwang@users.noreply.github.com>
2024-04-03 00:31:48 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 664 ms, Memory: 65156 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,37 +1,30 @@
 import sys
+input=sys.stdin.readline
 sys.setrecursionlimit(10000)
-input = sys.stdin.readline
 
-n, m = map(int, input().split())
-posco = [[]for _ in range(n+1)]
-asml = [False]* (n+1)
+n,m = map(int,input().split())
+hdc = [[] for _ in range(n+1)]
+visited = [False]*(n+1)
 
-def DFS(v):
-    asml[v]= True
-    for i in posco[v]:
-        if not asml[i]:
-            DFS(i)
-            
-            
+def dfs(v):
+    visited[v]=True
+    for i in hdc[v]:
+        if not visited[i]:
+            dfs(i)
+
+
 for _ in range(m):
-    s,e = map(int, input().split())
-    posco[s].append(e)
-    posco[e].append(s)
-    
+    s,e = map(int,input().split())
+    hdc[s].append(e)
+    hdc[e].append(s)
+
 count = 0
 
 for i in range(1,n+1):
-    if not asml[i]:
-        count +=1
-        DFS(i)
-        
-print(count)        
-            
-        
-    
-    
-    
-    
+    if not visited[i]:
+        count+=1
+        dfs(i)
+print(count)    
     
     
     
