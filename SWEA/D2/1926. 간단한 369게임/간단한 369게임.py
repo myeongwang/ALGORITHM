1e36e205fe523b645d9236c2188d156a0a1dc347
PLUTO <46295610+myeongwang@users.noreply.github.com>
2024-05-01 15:26:23 +0900

    [D2] Title: 간단한 369게임, Time: 102 ms, Memory: 44,708 KB -BaekjoonHub

---
+++ SWEA/D2/1926. 간단한 369게임/간단한 369게임.py
@@ -0,0 +1,11 @@
+N = int(input())
+clap = ['3', '6', '9']
+
+for i in range(1, N+1):
+    count = 0
+    for j in str(i):
+        if j in clap:
+            count += 1
+    if count > 0:
+        i = '-' * count
+    print( i, end=' ' )
